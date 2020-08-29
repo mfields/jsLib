e9dbe1c623f319a0dfd099b4e0e700c942ec4dd7
mfields <468369+mfields@users.noreply.github.com>
2020-08-29 14:49:52 -0700

    docs(internal): add docblock for makeInstanceOf()

--- .internal/makeInstanceOf.js
+++ .internal/makeInstanceOf.js
@@ -1,3 +1,13 @@
+/**
+ * Create an instance of a given constructor.
+ *
+ * @param {function} constructor The function which will be used to create
+ *   the object.
+ * @param {object} args An array-like object that contains iterable properties.
+ *   These properties will be passed as arguments to the constructor function.
+ *
+ * @return object
+ */
 export function makeInstanceOf (constructor, args) {
   args = Array.prototype.slice.call(args)
   var Maker = Function.prototype.bind.apply(constructor, [null].concat(args))
